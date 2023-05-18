f441db14ffc0176c55c3421f02b36cc754a9ff13
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2023-05-18 22:52:33 +0530

    item page updated

--- AtelierFrontEnd/src/app/item-page/item-page.component.ts
+++ AtelierFrontEnd/src/app/item-page/item-page.component.ts
@@ -2,17 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import axios from 'axios';
 import { Product } from 'src/service/product';
+import { UtilsServiceService } from '../utils/utils-service.service';
+import { UserService } from '../login/user.service';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-item-page',
   templateUrl: './item-page.component.html',
-  styleUrls: ['./item-page.component.css']
+  styleUrls: ['./item-page.component.css'],
+  providers: [MessageService]
 })
 export class ItemPageComponent implements OnInit {
   public itemId:any;
   public cake:any;
   public cakes:Product[]=[]
   public rndCake:any[]=[]
-  constructor(private route: ActivatedRoute) { }
+  public cart:any[]=[];
+  public userparsed:any;
+  constructor(private messageService: MessageService,public utilsServiceService : UtilsServiceService,private route: ActivatedRoute,public userService:UserService) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(async params => {
@@ -21,6 +27,14 @@ export class ItemPageComponent implements OnInit {
       this.getcakeById()
       this.getCakesList()
     });
+    this.utilsServiceService.cartObj$.subscribe(cart => {
+      this.cart= cart;
+
+    });
+    this.userService.userCast.subscribe(usr=>{
+      //console.log("user data" , usr)
+      this.userparsed = usr;
+    })
   }
 
   getcakeById(){
@@ -57,4 +71,31 @@ export class ItemPageComponent implements OnInit {
   getMoreProduct(){
     this.selectRandomElements(this.cakes,4)
   }
+
+  async addTocart(){
+    if(this.userparsed){
+
+    if(this.cart.length!=0){
+      if(this.cart.find(element => element.id === this.cake.id)){
+        console.log('present')
+        this.cart.find(element => element.id === this.cake.id).quantity+=1
+      }else{
+        this.cake.quantity=1
+        this.cart.push(this.cake)
+      }
+    }else{
+      this.cake.quantity=1
+      this.cart.push(this.cake)
+    }
+
+    this.utilsServiceService.setCartObj(this.cart)
+    this.messageService.add({ severity: 'success', summary: 'Accepted', detail: "Item Added to cart" });
+    await axios.post('addToCart',{data: this.cart}).then(res=>{
+
+    }).catch(err=>console.log(err))
+  }else{
+    this.messageService.add({ severity: 'warn', summary: 'Login/Signup', detail: "You have to login/signup to add-to-cart" });
+  }
+  }
+
 }
