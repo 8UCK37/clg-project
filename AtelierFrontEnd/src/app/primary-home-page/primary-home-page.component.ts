620401ba2bc93b38d80eafcbde19969b19fa77e7
Tanmoy-Biswas-2826 <tanmoybiswas2826@gmail.com>
2023-05-19 01:30:50 +0530

    carosel

--- AtelierFrontEnd/src/app/primary-home-page/primary-home-page.component.ts
+++ AtelierFrontEnd/src/app/primary-home-page/primary-home-page.component.ts
@@ -23,6 +23,14 @@ export class PrimaryHomePageComponent implements OnInit {
   public timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   public cakes:Product[]=[]
   public cart:any;
+  public carosleUrl:string=''
+  public urlarray:string[]=['https://firebasestorage.googleapis.com/v0/b/arachnoid-a42069.appspot.com/o/Cakes%2F52d421e6-5880-4074-a25e-eb70662d2159.jpg?alt=media&token=6febc586-490f-40f2-8400-64009ce1a02c',
+  'https://firebasestorage.googleapis.com/v0/b/arachnoid-a42069.appspot.com/o/Cakes%2Fe1973e37-999c-4807-87fc-8279e981e955.jpg?alt=media&token=6febc586-490f-40f2-8400-64009ce1a02c',
+'https://firebasestorage.googleapis.com/v0/b/arachnoid-a42069.appspot.com/o/Cakes%2Fc763e5cc-84e3-4213-b571-20a72ee9e24e.jpg?alt=media&token=6febc586-490f-40f2-8400-64009ce1a02c']
+
+
+
+
   responsiveOptions: any=[
     {
       breakpoint: '1199px',
@@ -59,7 +67,10 @@ export class PrimaryHomePageComponent implements OnInit {
       this.cart= cart;
       //console.log(this.cart)
     });
-
+      setInterval(() => {
+        this.selectRandomElements(this.urlarray,1)
+        console.log(this.carosleUrl)
+      }, 1500);
    }
 
   toggleMenu() {
@@ -86,7 +97,29 @@ export class PrimaryHomePageComponent implements OnInit {
       this.utilsServiceService.setCartObj(res.data.items)
     }).catch(err=>console.log(err))
   }
+
+
+  selectRandomElements(array: any[], count: number) {
+    const shuffled = [...array]; // Create a shallow copy of the original array
+    const result = [];
+
+    // Shuffle the elements using the Fisher-Yates algorithm
+    for (let currentIndex = shuffled.length - 1; currentIndex > 0; currentIndex--) {
+      const randomIndex = Math.floor(Math.random() * (currentIndex + 1));
+      [shuffled[currentIndex], shuffled[randomIndex]] = [shuffled[randomIndex], shuffled[currentIndex]];
+    }
+
+    // Select the first 'count' elements from the shuffled array
+    for (let i = 0; i < count; i++) {
+      result.push(shuffled[i]);
+    }
+
+    this.carosleUrl=result[0];
+  }
+
+
 }
+
 
 
 
