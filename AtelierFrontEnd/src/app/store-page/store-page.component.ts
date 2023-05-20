136e64c6798e1c28df16f9ff72be8139e0487b4f
Tanmoy-Biswas-2826 <tanmoybiswas2826@gmail.com>
2023-05-20 08:16:18 +0530

    scrolling

--- AtelierFrontEnd/src/app/store-page/store-page.component.ts
+++ AtelierFrontEnd/src/app/store-page/store-page.component.ts
@@ -19,7 +19,7 @@ export class StorePageComponent implements OnInit {
   public priceOptions:string[]=['All','Under 700','Between 700 and 1000','1000 and above']
   public selectedPriceRange:any;
 
-  public tagOptions:string[]=['Birthday','Anniversary']
+  public tagOptions:string[]=['Birthday','Anniversary','Kids','Cup Cake']
   public selectedTag:any;
   constructor(private router: Router,private route: ActivatedRoute) { }
 
@@ -45,7 +45,7 @@ export class StorePageComponent implements OnInit {
       if(res.data.length!=0){
       this.searchResults=res.data;
       this.filteredResults=res.data
-      this.header='Showing results for '+ this.searchTerm
+      this.header='Showing results for '+ this.searchTerm +' Cakes'
       }else{
         this.header='No matching results found for "'+this.searchTerm+'" ,you can try these instead '
         this.getCakesList();
